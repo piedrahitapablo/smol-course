889d927d11c5a9a9ab3ca96135d05dba357bad3e
piedrahitapablo <juan.p@snappr.com>
2025-03-09 16:52:24 -0500

    complete sft

---
+++ 1_instruction_tuning/notebooks/sft.py
@@ -0,0 +1,61 @@
+# Import necessary libraries
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from trl import SFTConfig, SFTTrainer, setup_chat_format
+import torch
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+# Load the model and tokenizer
+model_name = "HuggingFaceTB/SmolLM2-135M"
+model = AutoModelForCausalLM.from_pretrained(
+    pretrained_model_name_or_path=model_name
+).to(device)
+tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
+
+# Set up the chat format
+model, tokenizer = setup_chat_format(model=model, tokenizer=tokenizer)
+
+# TODO: define your dataset and config using the path and name parameters
+ds = load_dataset(path="HuggingFaceTB/smoltalk", name="everyday-conversations")
+
+# Set our name for the finetune to be saved &/ uploaded to
+finetune_name = "SmolLM2-FT-smoltalk"
+finetune_tags = ["smol-course", "module_1"]
+
+# Configure the SFTTrainer
+sft_config = SFTConfig(
+    output_dir="./sft_output",
+    max_steps=1000,  # Adjust based on dataset size and desired training duration
+    per_device_train_batch_size=4,  # Set according to your GPU memory capacity
+    learning_rate=5e-5,  # Common starting point for fine-tuning
+    logging_steps=10,  # Frequency of logging training metrics
+    save_steps=100,  # Frequency of saving model checkpoints
+    evaluation_strategy="steps",  # Evaluate the model at regular intervals
+    eval_steps=50,  # Frequency of evaluation
+    use_mps_device=(
+        True if device == "mps" else False
+    ),  # Use MPS for mixed precision training
+    hub_model_id=finetune_name,  # Set a unique name for your model
+    dataset_text_field="messages",
+)
+
+# Initialize the SFTTrainer
+trainer = SFTTrainer(
+    model=model,
+    args=sft_config,
+    train_dataset=ds["train"],
+    tokenizer=tokenizer,
+    eval_dataset=ds["test"],
+)
+
+trainer.train()
+
+# Save the model
+trainer.save_model(f"./{finetune_name}")
